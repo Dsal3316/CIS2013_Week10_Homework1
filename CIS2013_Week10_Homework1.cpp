24223847c637d79e1509b1ebd8f9ad86d5f29d81
Dsal3316 <35541000+Dsal3316@users.noreply.github.com>
2018-03-29 23:15:00 -0500

    Initail board setup

---
+++ CIS2013_Week10_Homework1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+	
+	int x,y;
+	int bombs =0;
+	int hight;
+	int width;
+	
+	cout << "Please enter board Height: " << endl;
+	cin >> hight;
+	cout << "Please enter board Width: " << endl;
+	cin >> width;
+	cout << "Please enter number of bombs: " << endl;
+	cin >> bombs;
+	
+	
+	return 0;
+}
