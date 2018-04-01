f4af95417c42706a82e2973cc7ce76e324acafaf
Dsal3316 <35541000+Dsal3316@users.noreply.github.com>
2018-04-01 00:44:37 -0500

    total crap but at least compiles

--- CIS2013_Week10_Homework1.cpp
+++ CIS2013_Week10_Homework1.cpp
@@ -1,20 +1,46 @@
 #include <iostream>
-#include <string>
+#include <cstring>
 using namespace std;
+
+typedef int* IntArrayPtr;
 
 int main(){
 	
+	//bool gameover=false;
 	int x,y;
 	int bombs =0;
-	int hight;
-	int width;
+	//srand((unsigned)time(0));
 	
-	cout << "Please enter board Height: " << endl;
-	cin >> hight;
-	cout << "Please enter board Width: " << endl;
-	cin >> width;
-	cout << "Please enter number of bombs: " << endl;
-	cin >> bombs;
+	//while (gameover=false){
+	
+		cout << "Please enter board Height: " << endl;
+		cin >> x;
+		cout << "Please enter board Width: " << endl;
+		cin >> y;
+		cout << "Please enter number of bombs: " << endl;
+		cin >> bombs;
+		
+		IntArrayPtr *m = new IntArrayPtr[x];
+		int i,j;
+		for (i=0; i < x; i++)
+			m[i] = new int[y];
+		
+		cout << "       Welcome to Minesweeper." << endl;
+		for (i=0; i< x; i++){
+			for(j=0; j < y; j++)
+				cout << m[i][j] << " ";
+			cout << endl;
+		}
+	
+	
+	//}	
+		
+	
+	
+	
+	
+	
+	
 	
 	
 	return 0;
