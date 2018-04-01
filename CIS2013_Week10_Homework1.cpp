459750c3acbfa5e80d0ffecb54afecd443c573bf
Dsal3316 <35541000+Dsal3316@users.noreply.github.com>
2018-04-01 01:18:21 -0500

    another array idea

--- CIS2013_Week10_Homework1.cpp
+++ CIS2013_Week10_Homework1.cpp
@@ -1,39 +1,51 @@
 #include <iostream>
 #include <cstring>
+#include <time.h>
 using namespace std;
 
-typedef int* IntArrayPtr;
+void boardxprint (int i){
+	cout << i;
+	if (i<10){
+		cout << " ";
+	}
+}
+	
+void boardyprint (int i){
+	cout<< "__";
+}
 
 int main(){
 	
 	//bool gameover=false;
-	int x,y;
+	int boardx,boardy;
 	int bombs =0;
-	//srand((unsigned)time(0));
+	int x,y;
+	srand(time(null));
 	
 	//while (gameover=false){
 	
 		cout << "Please enter board Height: " << endl;
-		cin >> x;
+		cin >> boardx;
 		cout << "Please enter board Width: " << endl;
-		cin >> y;
+		cin >> boardy;
 		cout << "Please enter number of bombs: " << endl;
 		cin >> bombs;
 		
-		IntArrayPtr *m = new IntArrayPtr[x];
-		int i,j;
-		for (i=0; i < x; i++)
-			m[i] = new int[y];
-		
-		cout << "       Welcome to Minesweeper." << endl;
-		for (i=0; i< x; i++){
-			for(j=0; j < y; j++)
-				cout << m[i][j] << " ";
-			cout << endl;
+		cout << "       Time to play minesweeper." << endl;
+		cout << endl << "                        ";
+		for (int x =0; x <boardx; x++){
+				boardxprint(x);
 		}
+		cout << endl;
+		for (int y=0; y < boardy; y++){
+			boardyprint(x);
+		}
+			
+		
+		
 	
 	
-	//}	
+	
 		
 	
 	
